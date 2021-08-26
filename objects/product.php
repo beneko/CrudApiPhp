bac47f2804d5bfc8236806eba365a1e39786a699
beneko <behzadnekouei@gmail.com>
2021-08-26 11:28:48 +0200

    method create added to the class product and product creation tested with postman

--- objects/product.php
+++ objects/product.php
@@ -39,4 +39,34 @@ class Product {
 
         return $stmt;
     }
+
+    public function create() {
+        // requête d'insertion
+        $query = "INSERT INTO
+                " . $this->table_name . "
+            SET
+                name=:name, price=:price, description=:description, category_id=:category_id, created=:created";
+        // on prépare la requête
+        $stmt = $this->conn->prepare($query);
+
+        //nettoyage
+        $this->name = htmlspecialchars(strip_tags($this->name));
+        $this->price = htmlspecialchars(strip_tags($this->price));
+        $this->description = htmlspecialchars(strip_tags($this->description));
+        $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+        $this->created = htmlspecialchars(strip_tags($this->created));
+
+        // on bind
+        $stmt->bindParam(":name", $this->name);
+        $stmt->bindParam(":price", $this->price);
+        $stmt->bindParam(":description", $this->description);
+        $stmt->bindParam(":category_id", $this->category_id);
+        $stmt->bindParam(":created", $this->created);
+
+        // on lance la requête
+        if ($stmt->execute()) {
+            return true;
+        }
+        return false;
+    }
 }
